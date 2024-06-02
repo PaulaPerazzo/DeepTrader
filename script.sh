f339f262dcbc9233910ca1510b045850dd22ef6e
paulaperazzo <mpps@cin.ufpe.br>
2024-06-02 14:26:02 -0300

    feat/test1: linux env

---
+++ script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=test_job_rl
+#SBATCH --ntasks=1
+#SBATCH --mem 32G
+#SBATCH -c 8
+#SBATCH -o job.log
+#SBATCH --output=job_output.txt
+#SBATCH --error=job_error.txt
+
+source /env/bin/activate
+
+python3 src/run.py -c hyper.json
